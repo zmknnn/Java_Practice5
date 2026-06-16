38519db4760ad91d9e5aca941917ee1cae8004d4
zmknnn <kotunonna@gmail.com>
2026-06-16 19:56:38 +0300

    nitial project

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.platform:junit-platform-suite")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
